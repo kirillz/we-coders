75b62806a625a3040e08b1e5eca19fba72e3e61c
icefox <kzasorin@gmail.com>
2020-11-08 01:34:31 +0300

    Интеграция mainpage_top_slider
    + исправил ошибки header.php

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -1,52 +1,68 @@
-<?php
-if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+<?
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
     die();
+}
+
 use Bitrix\Main\Page\Asset;
+
 ?>
 
 <!DOCTYPE html>
-<html class="no-js" lang="ru">
+<html class="no-js" lang="en">
 <head>
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+
     <title><? $APPLICATION->ShowTitle(); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="shortcut icon" type="image/x-icon" href="<?= SITE_TEMPLATE_PATH ?>/assets/img/favicon.png">
 
     <?php
-        Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/assets/css/bootstrap.min.css");
-        Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/assets/css/owl.carousel.css");
-        Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/assets/css/animate-text.css");
-        Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/assets/css/magnific-popup.css");
-        Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/assets/css/et-line.css");
-        Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/assets/css/pe-icon-7-stroke.css");
-        Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/assets/css/shortcode/shortcodes.css");
-        Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/assets/css/font-awesome.min.css");
-        Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/assets/style.css");
-        Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/assets/css/responsive.css");
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/css/bootstrap.min.css');
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/css/owl.carousel.css');
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/css/animate-text.css');
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/css/magnific-popup.css');
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/css/et-line.css');
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/css/pe-icon-7-stroke.css');
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/css/shortcode/shortcodes.css');
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/css/meanmenu.min.css');
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/css/font-awesome.min.css');
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/css/font-awesome.min.css');
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/style.css');
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/css/responsive.css');
 
-        Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/vendor/modernizr-2.8.3.min.js");
-        Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/bootstrap.min.js");
-        Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/owl.carousel.min.js");
-        Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/jquery.counterup.min.js");
-        Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/waypoints.min.js");
-        Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/jquery.magnific-popup.min.js");
-        Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/jquery.mixitup.min.js");
-        Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/jquery.meanmenu.js");
-        Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/jquery.nav.js");
-        Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/jquery.parallax-1.1.3.js");
-        Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/animate-text.js");
-        Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/plugins.js");
-        Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/assets/js/main.js");
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/vendor/modernizr-2.8.3.min.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/vendor/jquery-1.12.0.min.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/bootstrap.min.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/owl.carousel.min.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/jquery.counterup.min.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/waypoints.min.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/jquery.magnific-popup.min.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/jquery.mixitup.min.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/jquery.meanmenu.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/jquery.nav.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/jquery.parallax-1.1.3.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/animate-text.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/plugins.js');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/main.js');
 
-        Asset::getInstance()->addString('<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">')
+    Asset::getInstance()
+        ->addString('<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">');
     ?>
 
     <? $APPLICATION->ShowHead(); ?>
 </head>
 <body>
+
 <div id="panel">
-    <?$APPLICATION->ShowPanel();?>
+    <? $APPLICATION->ShowPanel(); ?>
 </div>
+
+<!--[if lt IE 8]>
+<p class="browserupgrade">Вы используете <strong>устаревший</strong> браузер. Пожалуйста
+    <a href="http://browsehappy.com/">обновите его</a>
+</p>
+<![endif]-->
 
 <!-- Линия с контактами -->
 <div class="header-top-area bg-color ptb-10 hidden-xs">
@@ -83,7 +99,7 @@ use Bitrix\Main\Page\Asset;
                     <!-- Логотип -->
                     <div class="col-md-2 col-sm-3 col-xs-8">
                         <div class="logo ptb-22">
-                            <a href="index.html">
+                            <a href="/">
                                 <img src="<?= SITE_TEMPLATE_PATH ?>/assets/img/logo/logo.png" alt="main logo">
                             </a>
                         </div>
