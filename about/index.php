c37e432eecb0ab7a04f78753b0f6574be410e29d
icefox <kzasorin@gmail.com>
2020-11-08 14:43:06 +0300

    О нас  - включаемые области

--- about/index.php
+++ about/index.php
@@ -1,11 +1,41 @@
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
-$APPLICATION->SetTitle("О нас");
-$APPLICATION->SetPageProperty('TITLE', 'О нас');
-$APPLICATION->SetPageProperty('keywords', 'о нас', 'we coders', 'сайты');
-$APPLICATION->SetPageProperty('description', 'Description ++');
+$APPLICATION->SetTitle('О нас');
+$APPLICATION->SetPageProperty('TITLE', 'О нас | We Coders');
+$APPLICATION->AddChainItem($APPLICATION->GetTitle(), $APPLICATION->GetCurDir());
+
+$APPLICATION->SetPageProperty("keywords", "о нас, о компании, we coders, сайты");
 ?>
-<p>О НАС</p>
+
+<!-- О нас -->
+<section class="who-area-are pad-90" id="about_us">
+    <div class="container">
+        <h2 class="title-1">
+            <? $APPLICATION->IncludeComponent("bitrix:main.include", "", [
+                "AREA_FILE_SHOW"   => "page",
+                "AREA_FILE_SUFFIX" => "about_title"
+            ]); ?>
+        </h2>
+        <div class="row">
+            <div class="col-md-7">
+                <div class="who-we">
+                    <? $APPLICATION->IncludeComponent("bitrix:main.include", "", [
+                        "AREA_FILE_SHOW"   => "page",
+                        "AREA_FILE_SUFFIX" => "about_text"
+                    ]); ?>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="about-bg">
+                    <? $APPLICATION->IncludeComponent("bitrix:main.include", "", [
+                        "AREA_FILE_SHOW"   => "page",
+                        "AREA_FILE_SUFFIX" => "about_image"
+                    ]); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
